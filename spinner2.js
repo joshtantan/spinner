63aa5463b1599df9d1539015ffbe840b7eebe4ab
Joshua Tan <josh.tan.tann@gmail.com>
2020-09-22 21:34:05 +0000

    Implement spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,9 @@
+const spinnerChars = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   ']
+
+for (let i = 0; i < spinnerChars.length; i++) {
+  const delay = 100 + (i * 200);
+
+  setTimeout(() => {
+    process.stdout.write(spinnerChars[i]);
+  }, delay);
+}
